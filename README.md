eb85467b9706b43774d47e79297a348ec3225503
Moritz Gottschling <gottschling.moritz@gmail.com>
2023-11-04 17:00:22 +0100

    Improve setup process

---
+++ README.md
@@ -0,0 +1,10 @@
+# Instructions
+
+1. Start by running `./install.sh 0`
+2. Clone `dotfiles` manually into `~/dotfiles`.
+3. Run `./install.sh 1`.
+4. Install `pacdef`.
+5. Import all necessary `pacdef` groups, e.g. `pacdef g import ~/dotfiles/packages/generic/*`.
+6. Install packages `pacdef p sy`.
+7. Run `./install.sh 2`.
+
--- install.sh
+++ install.sh
@@ -1,4 +1,29 @@
 #!/bin/bash
 
-ansible-playbook local.yml --ask-vault-pass --ask-become-pass
+export ANSIBLE_HOST_KEY_CHECKING=false
 
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 <step>"
+    exit 1
+fi
+
+step="$1"
+
+case "$step" in
+    0)
+        playbook="./0_initial.yml"
+        ;;
+    1)
+        playbook="./1_post-dotfiles-clone.yml"
+        ;;
+    2)
+        playbook="./2_post-packages.yml"
+        ;;
+    *)
+        echo "Invalid step: $step"
+        echo "Usage: $0 <step>"
+        exit 1
+        ;;
+esac
+
+ansible-playbook "$playbook" --ask-vault-pass --ask-become-pass
